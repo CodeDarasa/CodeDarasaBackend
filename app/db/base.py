079de602ddf4c321c6021f333afdd73bfad4cda5
sharonsyra <sharonwaithii@gmail.com>
2025-08-22 17:44:58 +0300

    Python versions

--- app/db/base.py
+++ app/db/base.py
@@ -5,8 +5,8 @@ Base = declarative_base()
 
 # Import all models so they are registered with Base.metadata
 # pylint: disable=unused-import, wrong-import-position
-from app.db.models import category  # noqa: F401
-from app.db.models import course    # noqa: F401
-from app.db.models import comment   # noqa: F401
-from app.db.models import rating    # noqa: F401
-from app.db.models import user      # noqa: F401
+from app.db.models.category import Category  # noqa: F401
+from app.db.models.course import Course      # noqa: F401
+from app.db.models.comment import Comment    # noqa: F401
+from app.db.models.rating import Rating      # noqa: F401
+from app.db.models.user import User          # noqa: F401
